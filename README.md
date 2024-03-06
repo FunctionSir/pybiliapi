570753c92938e986629a01b8926176573c60233d
FunctionSir <functionsir@outlook.com>
2024-03-06 17:28:28 +0800

    Fixed a terrible bug

--- README.md
+++ README.md
@@ -1,31 +1,41 @@
 # pybiliapi
-用API获取B站上的视频信息等等.  <br>
+
+用API获取B站上的视频信息等等.
+
 ## 使用方法
-简单粗暴地扔到你的程序文件目录, import之即可. 此模块无需使用pip获取,也无法使用pip获取.  <br>
-要想裁剪她同样简单粗暴,删掉不用的函数即可.  <br>
-把相关函数等拷贝至自己的程序也是可以的, 只要遵守许可证要求 (见LICENSE文件及README.md文件最下方的说明) .  <br>
+
+简单粗暴地扔到你的程序文件目录, import之即可. 此模块无需使用pip获取,也无法使用pip获取.  
+要想裁剪她同样简单粗暴,删掉不用的函数即可.  
+把相关函数等拷贝至自己的程序也是可以的, 只要遵守许可证要求 (见LICENSE文件及README.md文件最下方的说明) .  
+
 ## 函数说明
-bvid(aid) AV2BV str <br>
-aid(bvid) BV2AV int <br>
-cid(aid) 获取cid int <br>
-cover(aid) 获取封面URL str <br>
-view(aid) 获取观看量 int <br>
-video_p(aid) 分P数 int <br>
-like(aid) 获取赞数 int <br>
-favorite(aid) 获取收藏数 int <br>
-danmaku(aid) 获取弹幕数 int <br>
-coin(aid) 获取硬币数 int <br>
-share(aid) 获取分享数 int <br>
-reply(aid) 获取评论数 int <br>
-up_name(aid) 获取某视频的UP的名字 str <br>
-up_uid(aid) 获取某视频的UP的UID int <br>
-up_face(aid) 获取某视频的UP的头像（URL) str <br>
-xml_danmaku_url(aid) 获取XML弹幕文件的URL str <br>
-xml_danmaku_text(aid) 获取XML弹幕文件内容 str <br>
+
+注: aid是不包含"av"两字母的AV号, bvid是包括"BV"两字母的BV号.  
+bvid(aid) AV2BV str  
+aid(bvid) BV2AV int  
+cid(aid) 获取cid int  
+cover(aid) 获取封面URL str  
+view(aid) 获取观看量 int  
+video_p(aid) 分P数 int  
+like(aid) 获取赞数 int  
+favorite(aid) 获取收藏数 int  
+danmaku(aid) 获取弹幕数 int  
+coin(aid) 获取硬币数 int  
+share(aid) 获取分享数 int  
+reply(aid) 获取评论数 int  
+up_name(aid) 获取某视频的UP的名字 str  
+up_uid(aid) 获取某视频的UP的UID int  
+up_face(aid) 获取某视频的UP的头像（URL) str  
+xml_danmaku_url(aid) 获取XML弹幕文件的URL str  
+xml_danmaku_text(aid) 获取XML弹幕文件内容 str  
+
 ## 使用样例
+
 ````python
 import biliapi
-biliapi.view(ba.aid('BV17x411w7KC')) 
+biliapi.view(ba.aid('BV17x411w7KC'))
 ````
+
 ## 关于LGPL不同版本间及与GPL许可证之间的不兼容问题的说明
+
 特在此说明: 若您的项目使用LGPL v3, 或LGPL v2.1及之后的任何版本, 或GPL v2及之后的任何版本, 或AGPL v3及之后的任何版本时, 您可以视为此项目的许可证为LGPL v3, 或LGPL v2.1及之后的任何版本, 或GPL v2及之后的任何版本, 或AGPL v3及之后的任何版本(与您的项目的许可证相对应).
--- biliapi.py
+++ biliapi.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-#VER 1.0
+# VER 1.0
 import requests
 import random
 import json
@@ -19,73 +19,110 @@ s_ua = random.choice(ua)
 headers = {
     "User-Agent": s_ua
 }
+
+
 def bvid(aid):
     response = requests.get(url=base+'aid='+str(aid), headers=headers)
     jdata = json.loads(response.text)
     return jdata['data']['bvid']
+
+
 def aid(bvid):
     response = requests.get(url=base+'bvid='+bvid, headers=headers)
     jdata = json.loads(response.text)
     return jdata['data']['aid']
+
+
 def cid(aid):
     response = requests.get(url=base+'aid='+str(aid), headers=headers)
     jdata = json.loads(response.text)
     return jdata['data']['cid']
+
+
 def cover(aid):
     response = requests.get(url=base+'aid='+str(aid), headers=headers)
     jdata = json.loads(response.text)
     return jdata['data']['pic']
+
+
 def view(aid):
     response = requests.get(url=base+'aid='+str(aid), headers=headers)
     jdata = json.loads(response.text)
     return jdata['data']['stat']['view']
-def video_p(aid)
+
+
+def video_p(aid):
     response = requests.get(url=base+'aid='+str(aid), headers=headers)
     jdata = json.loads(response.text)
     return jdata['data']['videos']
+
+
 def like(aid):
     response = requests.get(url=base+'aid='+str(aid), headers=headers)
     jdata = json.loads(response.text)
     return jdata['data']['stat']['like']
+
+
 def favorite(aid):
     response = requests.get(url=base+'aid='+str(aid), headers=headers)
     jdata = json.loads(response.text)
     return jdata['data']['stat']['favorite']
+
+
 def danmaku(aid):
     response = requests.get(url=base+'aid='+str(aid), headers=headers)
     jdata = json.loads(response.text)
     return jdata['data']['stat']['danmaku']
+
+
 def coin(aid):
     response = requests.get(url=base+'aid='+str(aid), headers=headers)
     jdata = json.loads(response.text)
     return jdata['data']['stat']['coin']
+
+
 def share(aid):
     response = requests.get(url=base+'aid='+str(aid), headers=headers)
     jdata = json.loads(response.text)
     return jdata['data']['stat']['share']
+
+
 def reply(aid):
     response = requests.get(url=base+'aid='+str(aid), headers=headers)
     jdata = json.loads(response.text)
     return jdata['data']['stat']['reply']
+
+
 def up_name(aid):
     response = requests.get(url=base+'aid='+str(aid), headers=headers)
     jdata = json.loads(response.text)
     return jdata['data']['owner']['name']
+
+
 def up_uid(aid):
     response = requests.get(url=base+'aid='+str(aid), headers=headers)
     jdata = json.loads(response.text)
     return jdata['data']['owner']['mid']
+
+
 def up_face(aid):
     response = requests.get(url=base+'aid='+str(aid), headers=headers)
     jdata = json.loads(response.text)
     return jdata['data']['owner']['face']
+
+
 def xml_danmaku_url(aid):
-    #用F12工具箱可以轻松找到弹幕API需要感谢给了我寻找以提醒的【白zz】的【https://blog.csdn.net/u014788374/article/details/80367285】～
+    # 用F12工具箱可以轻松找到弹幕API需要感谢给了我寻找以提醒的【白zz】的【https://blog.csdn.net/u014788374/article/details/80367285】～
     response = requests.get(url=base+'aid='+str(aid), headers=headers)
     jdata = json.loads(response.text)
     return 'https://api.bilibili.com/x/v1/dm/list.so?oid='+str(jdata['data']['cid'])
+
+
 def xml_danmaku_text(aid):
-    #用F12工具箱可以轻松找到弹幕API需要感谢给了我寻找以提醒的【白zz】的【https://blog.csdn.net/u014788374/article/details/80367285】～
-    response = requests.get(url='https://api.bilibili.com/x/v1/dm/list.so?oid='+str(jdata['data']['cid']), headers=headers)
+    # 用F12工具箱可以轻松找到弹幕API需要感谢给了我寻找以提醒的【白zz】的【https://blog.csdn.net/u014788374/article/details/80367285】～
+    response = requests.get(url=base+'aid='+str(aid), headers=headers)
+    jdata = json.loads(response.text)
+    response = requests.get(
+        url='https://api.bilibili.com/x/v1/dm/list.so?oid='+str(jdata['data']['cid']), headers=headers)
     response.encoding = 'utf-8'
     return response.text
